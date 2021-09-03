8a90f58c6889f148061192d8b2759951e0550752
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-09-02 22:25:18 -0400

    s
    s

--- character.js
+++ character.js
@@ -1,8 +1,29 @@
-var character = {
-    name: Charles,
-    hobbies: ["mountaineering","video games","crafting cool stuff"],
-    friends: [],
-    major: "Computer Science"
+let x = 2;
+let y = 1;
+
+if (x > y) {
+    console.log("x is greater");
+} else {
+    console.log("y is greater");
+}
+var z = "reet";
+
+
+
+
+
+function thePerfectDrink(base,alc) {
+    //base and alc are ounces of liquid
+    //base should be coke or re bull
+    //alc is voka or whiskey
+    //if base:alc ratio < 3, dilution is required
+    let ratio = base/alc;
+    if (ratio < 3) {                                //decision -> is the ratio < 3?
+        console.log("dilute the solution!");            //prints 
+    } else {
+        console.log("Serve it up!");
+    }                                               //if the above decision is false,
+    return;                                             //prints 
 }
 
-
+thePerfectDrink(3,3);
