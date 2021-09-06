f3d8ae379e0994cc576a41b23014ba94e4099bdb
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-09-05 20:29:48 -0400

    Adding a like button
    like button implemented from existing code from reactjs.com

---
+++ LikeBtn.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styleSheet.css';
+
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
