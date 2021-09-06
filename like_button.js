91bd33f83f7d374112de5090c8fcb1e081a19e42
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-09-06 11:28:41 -0400

    s

--- like_button.js
+++ like_button.js
@@ -1,9 +1,13 @@
 counter = 0;
 var clickHandler = function(eventObject) {
   counter++;
-  document.querySelector("#likes").innerText = "Likes: " + counter;
+  document.querySelector("#likes").innerText = "Likes: " + counter;             //displays likes on html page
 }
 
-document.querySelector("#like_button").addEventListener("click",clickHandler);
+document.querySelector("#like_button").addEventListener("click",clickHandler);  //listens for clicks
 
+array1 = ["xcom","ycom","zcom"];
 
+map1 = array1.map(array1.toUpperCase());
+
+console.log(map1);
