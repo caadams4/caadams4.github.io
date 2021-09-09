52acb6cefcace144b05861d9abeb2ab6bfb51f6b
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-09-09 10:04:17 -0400

    CSS fun

--- like_button.js
+++ like_button.js
@@ -3,11 +3,12 @@
 counter = 0;
 var clickHandler = function(eventObject) {
   counter++;
+  console.log(eventObject);
   document.querySelector("#likes").innerText = "Likes: " + counter;             //displays likes on html page
 }
 
 document.querySelector("#like_button").addEventListener("click",clickHandler);  //listens for clicks
 
-document.getElementById(calc1).addEventListener("click")
+document.getElementById("#calc1").addEventListener("click")
 
 
