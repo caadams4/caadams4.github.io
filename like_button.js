48eee80f5ea8e6fa07f688637574c7f41ae978f4
caadams4 <79220528+caadams4@users.noreply.github.com>
2021-09-09 10:05:06 -0400

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -3,12 +3,9 @@
 counter = 0;
 var clickHandler = function(eventObject) {
   counter++;
-  console.log(eventObject);
+  alert("click registered")
   document.querySelector("#likes").innerText = "Likes: " + counter;             //displays likes on html page
 }
 
 document.querySelector("#like_button").addEventListener("click",clickHandler);  //listens for clicks
-
-document.getElementById("#calc1").addEventListener("click")
-
 
