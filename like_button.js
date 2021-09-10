11ceaa39fef0cd4491391c38bde602530cae571c
caadams4 <charlesaadams4@outlook.com>
2021-09-09 22:46:50 -0400

    Calculator Design
    Calculator design is pretty much complete. Next is adding interactivity to register inputs and calculations. I would like to complete that before Dr. Bart's lecture on React!

--- like_button.js
+++ like_button.js
@@ -1,12 +1,24 @@
 
 
 let counter = 0;
+
+let calcDisp = 0;
+let calcPow = 1;
+
+let calcHandler = function(eventObject) {
+  
+
+}
+
+
+
+
 var likeHandler = function(eventObject) {
   counter++;
   document.querySelector("#likes").innerText = "Likes: " + counter;             //displays likes on html page
 }
 document.querySelector("#like_button").addEventListener("click",likeHandler);  //listens for clicks
 
-
+document.querySelector(".calc_button").addEventListener("click",calcHandler);
 
 
