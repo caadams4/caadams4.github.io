fa3a38ccaa1bf1993e1bcc7c5a2e61844f2f58e2
caadams4 <charlesaadams4@outlook.com>
2021-09-15 16:24:46 -0400

    Calculator/LikeBtn updated
    Calc and Like Btn now JS OBJECTS, not abstractly programmed.

--- like_button.js
+++ like_button.js
@@ -1,7 +1,9 @@
 
 
 
-
+const LikeBtn = {
+  count: 0,
+}
 
 const Calculator = {
   display: "",
@@ -14,14 +16,17 @@ const Calculator = {
   mult: false,
   div: false,
   sign: false,
+  Eq: false,
+  dec: false,
 };
 
-let updateCalcDisp = function(calcTmp,calcTmp2) {
+let updateCalcDisp = function(calcTmp) {
   let opDisp = "";
   if (Calculator.plus === true) {opDisp = "+"; }
   else if (Calculator.minus === true) {opDisp = "-"; }
   else if (Calculator.mult === true) {opDisp = "*"; }
   else if (Calculator.div === true) {opDisp = "/"; }
+  Calculator.calcTmp = calcTmp;
   Calculator.display = Calculator.calcTmp + opDisp;
   if (Calculator.calcTmp2 !== null) {
     Calculator.display = "" + Calculator.calcTmp2 + opDisp + Calculator.calcTmp;
@@ -29,57 +34,59 @@ let updateCalcDisp = function(calcTmp,calcTmp2) {
   document.querySelector("#CalcDisplay").innerText = Calculator.display;
 }
 
-let calcInput = function(calcIn,calcTmp) {
-  let input = (calcTmp * 10) + calcIn;
-  console.log(input);
-  console.log(calcTmp);
+let calcInput = function(calcIn) {
+  if (Calculator.Eq === true) {
+    Calculator.calcTmp = 0;
+    Calculator.Eq = false;
+  } 
 
-  document.querySelector("#CalcDisplay").innerText = input;
-  Calculator.calcTmp = input;
-  updateCalcDisp(input,Calculator.calcTmp);
-  return input;
+  if (Calculator.dec === true) {
+    tmp = Calculator.calcTmp  + (calcIn * .1);
+    updateCalcDisp(tmp);
+  } else {
+    tmp = (Calculator.calcTmp * 10) + calcIn;
+    updateCalcDisp(tmp);
+  }
 }
 
 let calcInput1 = function(eventObject) {
-  console.log(1);
-  Calculator.calcTmp = calcInput(1,Calculator.calcTmp); 
-  console.log(Calculator.calcTmp)
+  calcInput(1); 
 }
 
 let calcInput2 = function(eventObject) {
-  Calculator.calcTmp = calcInput(2,Calculator.calcTmp); 
+  calcInput(2); 
 }
 
 let calcInput3 = function(eventObject) {
-  Calculator.calcTmp = calcInput(3,Calculator.calcTmp); 
+  calcInput(3); 
 }
 
 let calcInput4 = function(eventObject) {
-  Calculator.calcTmp = calcInput(4,Calculator.calcTmp); 
+  calcInput(4); 
 }
 
 let calcInput5 = function(eventObject) {
-  Calculator.calcTmp = calcInput(5,Calculator.calcTmp); 
+  calcInput(5); 
 }
 
 let calcInput6 = function(eventObject) {
-  Calculator.calcTmp = calcInput(6,Calculator.calcTmp); 
+  calcInput(6); 
 }
 
 let calcInput7 = function(eventObject) {
-  Calculator.calcTmp = calcInput(7,Calculator.calcTmp); 
+  calcInput(7); 
 }
 
 let calcInput8 = function(eventObject) {
-  Calculator.calcTmp = calcInput(8,Calculator.calcTmp); 
+  calcInput(8); 
 }
 
 let calcInput9 = function(eventObject) {
-  Calculator.calcTmp = calcInput(9,Calculator.calcTmp); 
+  calcInput(9); 
 }
 
 let calcInput0 = function(eventObject) {
-  Calculator.calcTmp = calcInput(0,Calculator.calcTmp); 
+  calcInput(0); 
 }
 
 let calcInputMinus = function(eventObject) {
@@ -87,7 +94,8 @@ let calcInputMinus = function(eventObject) {
   Calculator.calcTmp2 = Calculator.calcTmp;
   Calculator.calcTmp = "";
   Calculator.minus = true;
-  updateCalcDisp(Calculator.calcTmp2,Calculator.calcTmp)
+  Calculator.dec = false;
+  updateCalcDisp(Calculator.calcTmp)
 }
 
 let calcInputPlus = function(eventObject) {
@@ -95,74 +103,98 @@ let calcInputPlus = function(eventObject) {
   Calculator.calcTmp2 = Calculator.calcTmp;
   Calculator.calcTmp = "";
   Calculator.plus = true;
-  updateCalcDisp(Calculator.calcTmp2,Calculator.calcTmp)
+  Calculator.dec = false;
+  updateCalcDisp(Calculator.calcTmp)
 }
+
 let calcInputMult = function(eventObject) {
-if (Calculator.mult === true) {return;}
-Calculator.calcTmp2 = Calculator.calcTmp;
-Calculator.calcTmp = "";
-Calculator.mult = true;
-updateCalcDisp(Calculator.calcTmp2,Calculator.calcTmp)
+  if (Calculator.mult === true) {return;}
+  Calculator.calcTmp2 = Calculator.calcTmp;
+  Calculator.calcTmp = "";
+  Calculator.mult = true;
+  Calculator.dec = false;
+  updateCalcDisp(Calculator.calcTmp)
 }
+
 let calcInputDiv = function(eventObject) {
   if (Calculator.div === true) {return;}
   Calculator.calcTmp2 = Calculator.calcTmp;
   Calculator.calcTmp = "";
   Calculator.div = true;
-  updateCalcDisp(Calculator.calcTmp2,Calculator.calcTmp)
+  Calculator.dec = false;
+  updateCalcDisp(Calculator.calcTmp)
 }
 
 let calcInputEquals = function(eventObject) {
-  console.log(eventObject);
-  if (minus === true) {
-    calcTmp = calcTmp2 - calcTmp;
-    minus = false;
-    document.querySelector("#CalcDisplay").innerText = calcTmp;
-    calcTmp = 0;
+  Calculator.Eq = true;
+  Calculator.dec = false;
+  if (Calculator.minus === true) {
+    Calculator.calcTmp = Calculator.calcTmp2 - Calculator.calcTmp;
+    Calculator.calcTmp2 = null;
+    Calculator.minus = false;
+    updateCalcDisp(Calculator.calcTmp);
   }
-  if (plus === true) {
-    calcTmp = calcTmp2 + calcTmp;
-    plus = false;
-    document.querySelector("#CalcDisplay").innerText = calcTmp;
+  if (Calculator.plus === true) {
+    Calculator.calcTmp = Calculator.calcTmp2 + Calculator.calcTmp;
+    Calculator.calcTmp2 = null;
+    Calculator.plus = false;
+    updateCalcDisp(Calculator.calcTmp);
   }
-  if (mult === true) {
-    calcTmp = calcTmp2 * calcTmp;
-    mult = false;
-    document.querySelector("#CalcDisplay").innerText = calcTmp;
+  if (Calculator.mult === true) {
+    Calculator.calcTmp = Calculator.calcTmp2 * Calculator.calcTmp;
+    Calculator.calcTmp2 = null;
+    Calculator.mult = false;
+    updateCalcDisp(Calculator.calcTmp);
   }
-  if (div === true) {
-    calcTmp = calcTmp2 / calcTmp;
-    div = false;
-    document.querySelector("#CalcDisplay").innerText = calcTmp;
+  if (Calculator.div === true) {
+    Calculator.calcTmp = Calculator.calcTmp2 / Calculator.calcTmp;
+    Calculator.calcTmp2 = null;
+    Calculator.div = false;
+    updateCalcDisp(Calculator.calcTmp);
   }
 }
 
 let calcInputNeg = function(eventObject) {
-  calcTmp = calcTmp * -1;
-  document.querySelector("#CalcDisplay").innerText = calcTmp;
+  if (Calculator.plus === true) {
+    alert("going nowhere")
+    Calculator.plus = false;
+    Calculator.minus = true;
+    updateCalcDisp(Calculator.calcTmp)
+  } else if (Calculator.minus === true) {
+    alert("going somewhere")
+    Calculator.minus = false;
+    Calculator.plus = true;
+    updateCalcDisp(Calculator.calcTmp)
+  } else if (Calculator.mult === true || Calculator.div === true) {
+    alert("going dumb")
+    Calculator.calcTmp2 *= -1;
+    updateCalcDisp(Calculator.calcTmp);
+  }
 }
 
 let calcInputClear = function(eventObject) {
-  calcTmp = 0;
-  document.querySelector("#CalcDisplay").innerText = calcTmp;
+    Calculator.calcTmp = "";
+    updateCalcDisp(Calculator.calcTmp);
 }
+
 let calcInputAllClear = function(eventObject) {
-  calcTmp = 0;
-  calcTmp2 = 0;
-  plus,minus,mult,div = false;
-  document.querySelector("#CalcDisplay").innerText = calcTmp;
+  Calculator.calcTmp2 = null;
+  Calculator.calcTmp = "";
+  Calculator.plus,Calculator.minus,Calculator.mult,Calculator.div = false;
+  Calculator.display = "";
+  document.querySelector("#CalcDisplay").innerText = Calculator.display;
 }
 
-
-
-
+let calcInputDec = function(eventObject) {
+  Calculator.dec = true;
+}
 
 //document.querySelector("#CalcDisplay").innerText = calcTmp;  
 
 var likeHandler = function(eventObject) {
   console.log(eventObject);
-  counter++;
-  document.querySelector("#likes").innerText = "Likes: " + counter;             //displays likes on html page
+  LikeBtn.count++;
+  document.querySelector("#likes").innerText = "Likes: " +  LikeBtn.count;             //displays likes on html page
 }
 document.querySelector("#like_button").addEventListener("click",likeHandler);
 
@@ -183,22 +215,9 @@ document.querySelector("#calcDiv").addEventListener("click",calcInputDiv);
 document.querySelector("#calcPlus").addEventListener("click",calcInputPlus);
 document.querySelector("#calcMult").addEventListener("click",calcInputMult);
 document.querySelector("#calcNeg").addEventListener("click",calcInputNeg);
-//document.querySelector("#calcDec").addEventListener("click",calcInputDec);
+document.querySelector("#calcDec").addEventListener("click",calcInputDec);
 document.querySelector("#calcClear").addEventListener("click",calcInputClear);
 document.querySelector("#calcAllClear").addEventListener("click",calcInputAllClear);
-
-
-
-//calcInput(calcIn,calcTmp) 
-
-
-
-
-
-
-
-
-  //listens for clicks
 
 
 
